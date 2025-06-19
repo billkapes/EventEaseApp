523d397d7080496ce800fe236a353f801fafe330
billkapes <kapes15@yahoo.com>
2025-06-19 03:53:03 -0500

    Enhance Event Manager form with validation and structured layout

--- Models/Event.cs
+++ Models/Event.cs
@@ -1,10 +1,20 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace EventEaseApp.Models
 {
     public class Event
     {
+        [Required(ErrorMessage = "Name is required")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
         public string Description { get; set; }
-        public DateTime Date { get; set; }
+
+        [Required(ErrorMessage = "Date is required")]
+        public DateTime Date { get; set; } = DateTime.Today;
+
+        [Required(ErrorMessage = "Location is required")]
         public string Location { get; set; }
     }
 }
