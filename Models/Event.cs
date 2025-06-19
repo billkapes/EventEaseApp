d9581edba6ac2a24bb550b871c1839a82fb2de4f
billkapes <kapes15@yahoo.com>
2025-06-18 23:09:04 -0500

    Add Event model, Event service, and Event Manager page

---
+++ Models/Event.cs
@@ -0,0 +1,10 @@
+namespace EventEaseApp.Models
+{
+    public class Event
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public string Location { get; set; }
+    }
+}
---
+++ Services/EventService.cs
@@ -0,0 +1,26 @@
+using System.Collections.Generic;
+using System.Linq;
+using EventEaseApp.Models;
+
+namespace EventEaseApp.Services
+{
+    public class EventService
+    {
+        private readonly List<Event> _events = new();
+
+        public IEnumerable<Event> GetEvents()
+        {
+            return _events;
+        }
+
+        public void AddEvent(Event newEvent)
+        {
+            _events.Add(newEvent);
+        }
+
+        public void DeleteEvent(Event eventToDelete)
+        {
+            _events.Remove(eventToDelete);
+        }
+    }
+}
