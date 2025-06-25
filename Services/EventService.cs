38636f5482a8e8a9a6e8cc46afe5017e4bca54b3
billkapes <kapes15@yahoo.com>
2025-06-25 00:30:18 -0500

    Refactor EventService to use Blazored.LocalStorage for event persistence and update methods to be asynchronous

--- Services/EventService.cs
+++ Services/EventService.cs
@@ -1,26 +1,52 @@
 using System.Collections.Generic;
 using System.Linq;
 using EventEaseApp.Models;
+using Blazored.LocalStorage;
+using System.Threading.Tasks;
 
 namespace EventEaseApp.Services
 {
     public class EventService
     {
-        private readonly List<Event> _events = new();
+        private List<Event> _events = new();
+        private readonly ILocalStorageService _localStorage;
+        private const string StorageKey = "events";
 
-        public IEnumerable<Event> GetEvents()
+        public EventService(ILocalStorageService localStorage)
+        {
+            _localStorage = localStorage;
+        }
+
+        public async Task InitializeAsync()
+        {
+            await LoadEvents();
+        }
+
+        public List<Event> GetEvents()
         {
             return _events;
         }
 
-        public void AddEvent(Event newEvent)
+        public async Task AddEvent(Event newEvent)
         {
             _events.Add(newEvent);
+            await SaveEvents();
         }
 
-        public void DeleteEvent(Event eventToDelete)
+        public async Task DeleteEvent(Event eventToDelete)
         {
             _events.Remove(eventToDelete);
+            await SaveEvents();
+        }
+
+        private async Task SaveEvents()
+        {
+            await _localStorage.SetItemAsync(StorageKey, _events);
+        }
+
+        private async Task LoadEvents()
+        {
+            _events = await _localStorage.GetItemAsync<List<Event>>(StorageKey) ?? new List<Event>();
         }
 
         public void RegisterUser(Event evt, string userName)
